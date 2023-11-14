13dd05a62fc57244ee8ce5b65d7ccf7305bfc7a6
joelproxi <joeledmond95@gmail.com>
2023-11-14 15:39:01 +0100

    add comment table

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 from accounts.models import UserModel
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
-        ordering = ('-name')
+        ordering = ('-name',)
 
 
 class Post(models.Model):
@@ -22,7 +23,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/post/')
     category = models.ForeignKey(Category,
                                  on_delete=models.CASCADE,
-                                 related_name='posts')
+                                 related_name='categories')
     status = models.CharField(
         max_length=1,
         choices=POST_STATUS,
@@ -31,7 +32,25 @@ class Post(models.Model):
     author = models.ForeignKey(
        UserModel,
        on_delete=models.CASCADE,
-       related_name='users'
+       related_name='posted'
     )
     cretead = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+
+class comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='posts')
+    author = models.ForeignKey(UserModel,
+                               on_delete=models.CASCADE,
+                               related_name='commented')
+    content = models.TextField()
+    cretead = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)

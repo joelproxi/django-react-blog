cc67a4d82cc002e4fd07eb90d443d865a260a55b
joelproxi <joeledmond95@gmail.com>
2023-11-14 15:34:48 +0100

    add post table

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from accounts.models import UserModel
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +10,28 @@ class Category(models.Model):
         ordering = ('-name')
 
 
+class Post(models.Model):
+    DRAFT = 'D'
+    PUBLISHED = 'P'
+    POST_STATUS = (
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/post/')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='posts')
+    status = models.CharField(
+        max_length=1,
+        choices=POST_STATUS,
+        default=DRAFT
+    )
+    author = models.ForeignKey(
+       UserModel,
+       on_delete=models.CASCADE,
+       related_name='users'
+    )
+    cretead = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
